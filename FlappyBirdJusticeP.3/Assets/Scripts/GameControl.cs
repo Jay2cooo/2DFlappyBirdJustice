f3edb782f5e5af749984f5193bcc63e27abaed81
Jay2cooo <Onree.croffie@gmail.com>
2023-12-14 11:22:22 -0800

    Finally Finished the project
    Finally Finished the project

--- FlappyBirdJusticeP.3/Assets/Scripts/GameControl.cs
+++ FlappyBirdJusticeP.3/Assets/Scripts/GameControl.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
-public class GameControl : MonoBehaviour { 
+public class GameControl : MonoBehaviour
+{
 
     public static GameControl instance;
     public GameObject gameOverText;
@@ -16,7 +17,15 @@ public class GameControl : MonoBehaviour {
 
     private int score = 0;
 
+    AudioSource audiosource;
+    public AudioClip scoreSound;
+
     // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+
+    }
     void Awake()
     {
         if (instance == null)
@@ -37,19 +46,24 @@ public class GameControl : MonoBehaviour {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
-    
+
     public void BirdScored()
     {
-        if(gameOver)
+        if (gameOver)
         {
             return;
         }
         score++;
         scoreText.text = "Score: " + score.ToString();
+        PlaySound(scoreSound);
     }
     public void BirdDied()
     {
         gameOverText.SetActive(true);
         gameOver = true;
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        audiosource.PlayOneShot(clip);
     }
 }
--- FlappyBirdJusticeP.3/Assets/Scripts/bird.cs
+++ FlappyBirdJusticeP.3/Assets/Scripts/bird.cs
@@ -9,9 +9,14 @@ public class bird : MonoBehaviour {
     private bool isdDead = false;
     private Rigidbody2D rb2d;
     private Animator anim;
+
+    AudioSource audioSource;
+    public AudioClip flapSound;
+    public AudioClip dieSound;
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         rb2d = GetComponent<Rigidbody2D> ();
         anim = GetComponent<Animator> ();
     }
@@ -26,6 +31,7 @@ public class bird : MonoBehaviour {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2 (0, upForce));
                 anim.SetTrigger("Flap");
+                PlaySound(flapSound);
             }
         }
     }
@@ -35,5 +41,11 @@ public class bird : MonoBehaviour {
         isdDead = true;
         anim.SetTrigger("Die");
         GameControl.instance.BirdDied();
+        PlaySound(dieSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
